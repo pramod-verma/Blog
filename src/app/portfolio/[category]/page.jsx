0499cea307b1cab4076d8e4f685bfa906ebf5c79
Pramod Verma <pramod-verma@users.noreply.github.com>
2023-10-29 12:06:29 +0530

    portfolio page completed

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,27 @@
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 import React from 'react'
+import styles from "./page.module.css"
 
-const Category = () => {
+const Category = (params) => {
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.Category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Para</p>
+          <Button text='See more' url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+          fill={true}
+          src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+          alt="image"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
